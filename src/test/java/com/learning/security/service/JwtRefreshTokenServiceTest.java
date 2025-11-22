116fa2a141c0ca88b502c53ad284cafb3ed57d18
praneeth reddy <praneethls4008@gmail.com>
2025-11-22 19:21:29 +0530

    jwt unit test

---
+++ src/test/java/com/learning/security/service/JwtRefreshTokenServiceTest.java
@@ -0,0 +1,38 @@
+package com.learning.security.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JwtRefreshTokenServiceTest {
+
+    private final String SECRET_KEY = "41557e26a4b67c34f538023d1fbbf5601fbb73a364ffd13ecbabd30d6342dcd7";
+    private final long refreshTokenExpiration = 10;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void extractUsername() {
+    }
+
+    @Test
+    void extractExpiryDate() {
+    }
+
+    @Test
+    void extractClaim() {
+    }
+
+    @Test
+    void generateRefreshToken() {
+    }
+
+    @Test
+    void isTokenValid() {
+    }
+
+    @Test
+    void isTokenExpired() {
+    }
+}
